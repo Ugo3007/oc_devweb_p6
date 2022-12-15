f037261d9d63e8be5dc1feab3c40612a37f8ee75
GABRIEL Ugo <ugo.gabriel@etu.unistra.fr>
2022-12-15 23:07:52 +0100

    ➕ Installed multer and @types/multer ✨ Created middleware multer-config.ts to capture pictures sent in form and rename them properly

---
+++ backend/src/middlewares/multer-config.ts
@@ -0,0 +1,23 @@
+import multer from "multer";
+
+const MIME_TYPES = {
+    'image/jpg' : 'jpg',
+    'image/jpeg' : 'jpg',
+    'image/png': 'png'
+}
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, 'src/images')
+    },
+    filename: (req, file, callback) => {
+        const name = file.originalname
+
+            .split(' ').join('_')
+        // @ts-ignore
+        const extension = MIME_TYPES[file.mimetype] || 'jpg'
+        callback(null, Date.now() + '.' + extension)
+    }
+})
+
+module.exports = multer({storage}).single('image')
