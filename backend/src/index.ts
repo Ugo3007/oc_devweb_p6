ba372b56bd4780ee446ea5e446c52690a2ddca6a
GABRIEL Ugo <ugo.gabriel@etu.unistra.fr>
2022-12-15 21:42:25 +0100

    ➕ Added Mongoose ➕ Added Dotenv 🙈 Updated .gitignore 🔧 Updated configuration files

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,17 @@
 import express from 'express'
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+
 
 const app = express()
+
+dotenv.config();
+dotenv.config({path: `.env.local`, override: true});
+
+mongoose.connect('mongodb+srv://' + process.env.MONGOOSE_USERNAME + ':' + process.env.MONGOOSE_PASSWORD + process.env.MONGOOSE_DATABASE_ADDRESS)
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.listen(3000, () => {
     console.log("Connecté")
