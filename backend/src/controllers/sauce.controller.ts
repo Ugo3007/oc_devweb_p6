64355535210450523e82b5c7fca23f7600f254e0
GABRIEL Ugo <ugo.gabriel@etu.unistra.fr>
2022-12-16 14:39:14 +0100

    🚚 Stuffed route /api/sauces (GET) and corrected import for Sauce Model

--- backend/src/controllers/sauce.controller.ts
+++ backend/src/controllers/sauce.controller.ts
@@ -1,11 +1,18 @@
 import express from "express";
 import auth from "../middlewares/auth"
 import multer from "../middlewares/multer-config";
+import Sauce from "../models/Sauce.model";
 
 const router = express.Router()
 
 router.get('/', auth, (req, res) => {
-    return res.status(200).json({message: "Route not implemented"})
+    Sauce.find()
+        .then(sauces => {
+            return res.status(200).json({sauces})
+        })
+        .catch(error => {
+            // return res.status(400).json({error})
+        })
 })
 
 router.get('/:id', auth, (req, res) => {
