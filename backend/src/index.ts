00bfa77d96a0d808a983641f5fbba11601be1e99
GABRIEL Ugo <ugo.gabriel@etu.unistra.fr>
2022-12-16 14:13:37 +0100

    🛂 Added headers for CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,6 +16,13 @@ mongoose.connect('mongodb+srv://' + process.env.MONGOOSE_USERNAME + ':' + proces
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
 app.use('/api/auth', userController)
 app.use('/api/sauces', sauceController)
 app.use('/src/images', express.static(path.join(__dirname, 'images')))
