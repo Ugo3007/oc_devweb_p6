32303fe30b861f9167962a1078c386c155bb68e3
GABRIEL Ugo <ugo.gabriel@etu.unistra.fr>
2022-12-22 15:34:46 +0100

    🔧 Updated config of multer-config to match correct images directory path

--- backend/src/middlewares/multer-config.ts
+++ backend/src/middlewares/multer-config.ts
@@ -1,19 +1,17 @@
 import multer from "multer";
 
 const MIME_TYPES = {
-    'image/jpg' : 'jpg',
-    'image/jpeg' : 'jpg',
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpg',
     'image/png': 'png'
 }
 
 const storage = multer.diskStorage({
     destination: (req, file, callback) => {
-        callback(null, 'src/images')
+        callback(null, 'images')
     },
     filename: (req, file, callback) => {
-        const name = file.originalname
-
-            .split(' ').join('_')
+        const name = file.originalname.split(' ').join('_')
         // @ts-ignore
         const extension = MIME_TYPES[file.mimetype] || 'jpg'
         callback(null, Date.now() + '.' + extension)
