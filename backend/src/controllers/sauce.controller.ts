825268605811c7867e3f58662f184c908659acf3
GABRIEL Ugo <ugo.gabriel@etu.unistra.fr>
2022-12-22 15:36:31 +0100

    ⚡️ Corrected controllers and config for images

--- backend/src/controllers/sauce.controller.ts
+++ backend/src/controllers/sauce.controller.ts
@@ -10,7 +10,7 @@ const router = express.Router()
 router.get('/', auth, (req, res) => {
     Sauce.find()
         .then(sauces => {
-            return res.status(200).json({sauces})
+            return res.status(200).json(sauces)
         })
         .catch(error => {
             return res.status(400).json({error})
@@ -20,7 +20,7 @@ router.get('/', auth, (req, res) => {
 router.get('/:id', auth, (req, res) => {
     Sauce.findOne({_id: req.params.id})
         .then(sauce => {
-            return res.status(200).json({sauce})
+            return res.status(200).json(sauce)
         })
         .catch(error => {
             return res.status(200).json({error})
@@ -33,13 +33,12 @@ router.post('/', auth, multer, (req, res) => {
         const sauce = new Sauce({
             ...sauceObject,
             dislikes: 0,
-            imageUrl: `${req.protocol}://${req.get('host')}/src/images/${req.file?.filename}`,
+            imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file?.filename}`,
             likes: 0,
             userId: req.auth.userId,
             usersDisliked: [],
             usersLiked: [],
         })
-        console.log('coucou')
         sauce.save()
             .then(() => res.status(201).json({message: "Objet enregistré"}))
             .catch((error) => res.status(400).json({error}))
@@ -47,7 +46,30 @@ router.post('/', auth, multer, (req, res) => {
 )
 
 router.put('/:id', auth, multer, (req, res) => {
-    return res.status(200).json({message: "Route not implemented"})
+    const sauceObject = req.file ? {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : {
+        ...req.body
+    }
+
+    delete sauceObject.userId
+    Sauce.findOne({_id: req.params.id})
+        .then(sauce => {
+            if (sauce === null) return res.status(404).json({message: 'Sauce not found'})
+            if (sauce.userId !== req.auth.userId) {
+                res.status(401).json({message: 'Forbidden access'})
+            } else {
+                Sauce.updateOne({_id: req.params.id}, {...sauceObject, _id: req.params.id})
+                    .then(() => res.status(200).json({message: 'Objet modifié !'}))
+                    .catch(error => {
+                        return res.status(400).json({error})
+                    });
+            }
+        })
+        .catch((error) => {
+            return res.status(400).json({error})
+        })
 })
 
 router.delete('/:id', auth, (req, res) => {
@@ -58,13 +80,13 @@ router.delete('/:id', auth, (req, res) => {
                     return res.status(401).json({message: 'Forbidden access'})
                 } else {
                     const filename = sauce.imageUrl.split('/images/')[1];
-                    Sauce.deleteOne({_id: req.params.id})
-                        .then(() => {
-                            fs.unlink(`images/${filename}`, () => {
+                    fs.unlink(`images/${filename}`, () => {
+                        Sauce.deleteOne({_id: req.params.id})
+                            .then(() => {
+                                return res.status(204).json({message: "Objet supprimé."})
                             })
-                            return res.status(200).json({message: 'Objet supprimé'})
-                        })
                         .catch(error => res.status(401).json({error}))
+                    })
                 }
             } else {
                 return res.status(400).json({message: 'Error: Sauce === null'})
