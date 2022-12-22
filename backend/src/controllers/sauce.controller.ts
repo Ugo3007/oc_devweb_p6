c91fd2e0b92acc3fd7c16b4d4cad9f69a8ffbd3a
GABRIEL Ugo <ugo.gabriel@etu.unistra.fr>
2022-12-22 23:05:39 +0100

    Modified put controller to delete existing picture when being replaced by another picture Stuffed 'like' route

--- backend/src/controllers/sauce.controller.ts
+++ backend/src/controllers/sauce.controller.ts
@@ -40,8 +40,12 @@ router.post('/', auth, multer, (req, res) => {
             usersLiked: [],
         })
         sauce.save()
-            .then(() => res.status(201).json({message: "Objet enregistré"}))
-            .catch((error) => res.status(400).json({error}))
+            .then(() => {
+                return res.status(201).json({message: "Objet enregistré"})
+            })
+            .catch((error) => {
+                return res.status(400).json({error})
+            })
     }
 )
 
@@ -61,7 +65,15 @@ router.put('/:id', auth, multer, (req, res) => {
                 res.status(401).json({message: 'Forbidden access'})
             } else {
                 Sauce.updateOne({_id: req.params.id}, {...sauceObject, _id: req.params.id})
-                    .then(() => res.status(200).json({message: 'Objet modifié !'}))
+                    .then(() => {
+                        if (req.file) {
+                            const filename = sauce.imageUrl.split('/images/')[1];
+                            fs.unlink(`images/${filename}`, () => {
+                                console.log('Picture successfully deleted')
+                            })
+                        }
+                        res.status(200).json({message: 'Objet modifié !'})
+                    })
                     .catch(error => {
                         return res.status(400).json({error})
                     });
@@ -85,7 +97,7 @@ router.delete('/:id', auth, (req, res) => {
                             .then(() => {
                                 return res.status(204).json({message: "Objet supprimé."})
                             })
-                        .catch(error => res.status(401).json({error}))
+                            .catch(error => res.status(401).json({error}))
                     })
                 }
             } else {
@@ -99,7 +111,54 @@ router.delete('/:id', auth, (req, res) => {
 
 
 router.post('/:id/like', auth, (req, res) => {
-    return res.status(200).json({message: "Route not implemented"})
+    const userId = req.body.userId
+    const like = req.body.like
+    const sauceId = req.params.id
+
+    Sauce.findOne({_id: sauceId})
+        .then((sauce) => {
+            if (sauce !== null) {
+                switch (like) {
+                    case 1:
+                        if (sauce.usersLiked.includes(userId)) {
+                            return res.status(409).json({message: 'User has already liked the sauce'})
+                        }
+                        sauce.usersLiked.push(userId)
+                        sauce.likes = sauce.usersLiked.length
+                        break
+                    case 0:
+                        if (sauce.usersLiked.includes(userId)) {
+                            sauce.usersLiked.splice(sauce.usersLiked.indexOf(userId), 1)
+                            sauce.likes = sauce.usersLiked.length
+                        }
+                        if (sauce.usersDisliked.includes(userId)) {
+                            sauce.usersDisliked.splice(sauce.usersDisliked.indexOf(userId), 1)
+                            sauce.dislikes = sauce.usersDisliked.length
+                        }
+                        break
+                    case -1:
+                        if (sauce.usersDisliked.includes(userId)) {
+                            return res.status(409).json({message: 'User has already liked the sauce'})
+                        }
+                        sauce.usersDisliked.push(userId)
+                        sauce.dislikes = sauce.usersDisliked.length
+                        break
+                    default:
+                        res.status(406).json({message: 'like is not equal to 1, 0 or -1'})
+                        break
+                }
+                Sauce.updateOne({_id: req.params.id}, sauce)
+                    .then(() => {
+                        return res.status(200).json({message: 'Objet modifié !'})
+                    })
+                    .catch(error => {
+                        return res.status(400).json({error})
+                    });
+            }
+        })
+        .catch((error) => {
+            res.status(400).json({error: error})
+        })
 })
 
 export default router
